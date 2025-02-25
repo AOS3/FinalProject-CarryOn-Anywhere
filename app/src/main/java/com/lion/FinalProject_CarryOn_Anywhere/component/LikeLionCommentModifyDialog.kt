c7b090656398b6537fcaa1ff4a2f1df78b1d821a
user <localhost_8080@naver.com>
2025-02-25 09:51:03 +0900

    0225 09:50 khs - 댓글 기능 구현(추가, 삭제, 수정, 신고)

--- app/src/main/java/com/lion/FinalProject_CarryOn_Anywhere/component/LikeLionCommentModifyDialog.kt
+++ app/src/main/java/com/lion/FinalProject_CarryOn_Anywhere/component/LikeLionCommentModifyDialog.kt
@@ -1,6 +1,9 @@
 package com.lion.FinalProject_CarryOn_Anywhere.component
 
+import androidx.compose.foundation.BorderStroke
+import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
@@ -17,15 +20,15 @@ import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
 import androidx.compose.material3.OutlinedTextField
+import com.lion.FinalProject_CarryOn_Anywhere.ui.theme.MainColor
 
 
 @Composable
 fun LikeLionCommentModifyDialog(
     showDialog: MutableState<Boolean>,
     onConfirm: (String) -> Unit,
-    onDismiss: () -> Unit
+    onDismiss: () -> Unit,
 ) {
-    // 입력된 댓글 텍스트를 저장할 상태
     val commentText = remember { mutableStateOf("") }
 
     if (showDialog.value) {
@@ -33,7 +36,7 @@ fun LikeLionCommentModifyDialog(
             onDismissRequest = { showDialog.value = false },
             title = {
                 Text(
-                    text = "댓글 입력",
+                    text = "댓글 수정",
                     fontSize = 20.sp
                 )
             },
@@ -56,23 +59,45 @@ fun LikeLionCommentModifyDialog(
                 }
             },
             confirmButton = {
-                Button(
-                    onClick = {
-                        onConfirm(commentText.value)
-                        showDialog.value = false
-                    }
+                Row(
+                    modifier = Modifier.fillMaxWidth(),
+                    horizontalArrangement = Arrangement.SpaceEvenly,
+
                 ) {
-                    Text("확인")
-                }
-            },
-            dismissButton = {
-                OutlinedButton(
-                    onClick = {
-                        showDialog.value = false
-                        onDismiss()
-                    }
-                ) {
-                    Text("취소", color = Color.Blue)
+                    // 취소 버튼
+                    LikeLionFilledButton(
+                        horizontalPadding = 0.dp,
+                        fillWidth = false,
+                        containerColor = Color.Transparent,
+                        contentColor = MainColor,
+                        text = "취소",
+                        border = BorderStroke(1.dp, MainColor),
+                        onClick = {
+                            showDialog.value = false
+                            onDismiss()
+                        },
+                        cornerRadius = 5,
+                        buttonWidth = 120.dp,
+                        paddingBottom = 20.dp,
+                    )
+                    // 확인 버튼 (입력이 있을 때만 활성화)
+                    LikeLionFilledButton(
+                        horizontalPadding = 0.dp,
+                        fillWidth = false,
+                        containerColor = if (commentText.value.isNotBlank()) MainColor else Color.Gray,
+                        contentColor = Color.White,
+                        text = "확인",
+                        onClick = {
+                            if (commentText.value.isNotBlank()) {
+                                onConfirm(commentText.value)
+                                showDialog.value = false
+                            }
+                        },
+                        cornerRadius = 5,
+                        buttonWidth = 120.dp,
+                        paddingBottom = 20.dp,
+                    )
+
                 }
             }
         )
